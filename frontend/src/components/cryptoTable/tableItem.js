b05efcf5d07ab38e59294e215ba3e5772e869e57
Gokaku <naokjj@gmail.com>
2021-01-16 00:07:14 +0100

    set and balance switched

--- frontend/src/components/cryptoTable/tableItem.js
+++ frontend/src/components/cryptoTable/tableItem.js
@@ -11,14 +11,14 @@ function tableItem(props) {
             <button onClick={() => props.delCrypto(symbol)} className="delButton">
                 <p className="pX">X</p>
             </button>
-            <p style={{float:"right"}} >{value + "$"}</p>
+            <p style={{float:"right"}}> {value + "$"} </p>
             <div>
                 <p style={{float:"left"}}>{name}</p>
-                <p style={{float:"left",clear:"right",fontSize:"small"}} >{amount + " " + symbol.toUpperCase()}</p>
                 <div>
-                    <button className="addBButton">
+                    <button style={{clear: "right", margin: "0px", marginRight: "8px"}} className="addBButton">
                         <p className="pSet">Set</p>
                     </button>
+                    <p style={{float:"left",fontSize:"small"}} >{amount + " " + symbol.toUpperCase()}</p>
                 </div>
             </div>
         </div>
